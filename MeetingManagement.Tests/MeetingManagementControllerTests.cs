a8779c97517fe0eb549debb0b1d6a2ebb1ecf38c
Rahul R <rsquare28j.n@gmail.com>
2026-01-12 17:52:35 +0530

    Add unit tests

---
+++ MeetingManagement.Tests/MeetingManagementControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using MeetingManagement.Test;
+using MeetingTaskManagement.DataLayer.Interfaces;
+using MeetingTaskManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingManagement.Tests
+{
+    [TestClass]
+    public sealed class MeetingManagementControllerTests
+    {
+
+        private const string ValidStartDate = "2023-10-01T10:00:00Z";
+        private const string ValidEndDate = "2023-10-01T11:00:00Z";
+        private IMeetingRepository _meetingRepository;
+        public MeetingManagementControllerTests()
+        {
+            _meetingRepository = new MockMeetingRepository();
+        }
+
+        [TestMethod]
+        public void CreateMeetingTest_ValidStartAndEndDate()
+        {
+            var controller = new MeetingManagementController(_meetingRepository);
+            var meetingStart = DateTime.Now;
+            var meetingEnd = meetingStart;
+            var meeting = new Meeting
+            {
+                Title = "Project Kickoff",
+                Description = "Initial project kickoff meeting",
+                StartTime = meetingStart,
+                EndTime = meetingEnd,
+                Attendees = new List<string> { "abc", "xyz"}
+            };
+            var response = controller.Create(meeting);
+
+            var badRequestResult = (BadRequestObjectResult)response.Result;
+            Assert.IsNotNull(badRequestResult.Value);
+            //Result should be bad request due to start end date being the same
+            Assert.AreEqual(400, badRequestResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetMeetingTest_ValidMeetingId()
+        {
+            var controller = new MeetingManagementController(_meetingRepository);
+            var meetingStart = DateTime.Now;
+            var meetingEnd = meetingStart;
+            var meeting = new Meeting
+            {
+                Id = 1,
+                Title = "Project Kickoff",
+                Description = "Initial project kickoff meeting",
+                StartTime = meetingStart,
+                EndTime = meetingEnd,
+                Attendees = new List<string> { "abc", "xyz"}
+            };
+            var result = controller.Create(meeting);
+            var getResponse = controller.Get(1);
+
+            var okResult = (OkObjectResult)getResponse.Result;
+            Assert.IsNotNull(okResult.Value);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetMeetingTest_InvalidMeetingId()
+        {
+            var controller = new MeetingManagementController(_meetingRepository);
+            var meetingStart = DateTime.Now;
+            var meetingEnd = meetingStart;
+            var meeting = new Meeting
+            {
+                Id = 1,
+                Title = "Project Kickoff",
+                Description = "Initial project kickoff meeting",
+                StartTime = meetingStart,
+                EndTime = meetingEnd,
+                Attendees = new List<string> { "abc", "xyz"}
+            };
+            var result = controller.Create(meeting);
+            var getResponse = controller.Get(2);
+
+            var notFoundResult = (NotFoundObjectResult)getResponse.Result;
+            Assert.IsNotNull(notFoundResult.Value);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
+        }
+    }
+}
---
+++ MeetingManagement.Tests/MockMeetingRepository.cs
@@ -0,0 +1,41 @@
+﻿using MeetingTaskManagement.DataLayer.Interfaces;
+using MeetingTaskManagement.Models;
+
+namespace MeetingManagement.Test
+{
+    public class MockMeetingRepository : IMeetingRepository
+    {
+        public int CreateMeeting(Meeting meeting)
+        {
+            return 1;
+        }
+
+        public bool DeleteMeeting(int id)
+        {
+            return true;
+        }
+
+        public Meeting GetMeetingById(int id)
+        {
+            return new Meeting
+            {
+                Id = id,
+                Title = "Test",
+                Description = "Test",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(1),
+                Attendees = ["abc", "xyz"]
+            };
+        }
+
+        public IList<Meeting> GetMeetings()
+        {
+            return new List<Meeting> { GetMeetingById(1), GetMeetingById(2) };
+        }
+
+        public bool UpdateMeeting(Meeting meeting)
+        {
+            return true;
+        }
+    }
+}
